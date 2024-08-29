922a88b9dc3f26410e427ea9f80d12c9efbfa0de
0xafrogeek <0xafrogeek@users.noreply.github.com>
2024-08-29 23:57:27 +0000

    chore: add app config metadata

--- d2.config.js
+++ d2.config.js
@@ -1,6 +1,10 @@
 const config = {
     type: 'app',
-    author: 'TheTechAid',
+    author: 'The TechAid',
+    type: 'data-presenter',
+    title: 'Data Presenter',
+    description: 'Present data visualizations in slidehow and presentation mode',
+    minDHIS2Version: '2.37',
 
     entryPoints: {
         app: './src/App.js',
